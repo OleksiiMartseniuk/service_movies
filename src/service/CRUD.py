b33743c48b1007fd8e91c476344eee5733ebca67
Aleksey Martsenyk <martsenyk.l@gmail.com>
2022-04-23 14:08:49 +0300

    create ServiceCRUD

---
+++ src/service/CRUD.py
@@ -0,0 +1,34 @@
+from typing import TypeVar, Type, Optional
+
+from fastapi import HTTPException
+from pydantic import BaseModel
+from tortoise import models
+
+
+ModelType = TypeVar('ModelType', bound=models.Model)
+CreateSchemaType = TypeVar('CreateSchemaType', bound=BaseModel)
+UpdateSchemaType = TypeVar('UpdateSchemaType', bound=BaseModel)
+GetSchemaType = TypeVar('GetSchemaType', bound=BaseModel)
+
+
+class ServiceCRUD:
+    model: Type[ModelType]
+    create_schema: CreateSchemaType
+    update_schema: UpdateSchemaType
+    get_schema: GetSchemaType
+
+    async def get(self, **kwargs) -> Optional[GetSchemaType]:
+        return await self.get_schema.from_queryset_single(self.model.get(**kwargs))
+
+    async def create(self, schema, **kwargs) -> Optional[CreateSchemaType]:
+        obj = await self.model.create(**schema.dict(exclude_unset=True), **kwargs)
+        return await self.get_schema.from_tortoise_orm(obj)
+
+    async def update(self, schema, **kwargs) -> Optional[UpdateSchemaType]:
+        await self.model.filter(**kwargs).update(**schema.dict(exclude_unset=True))
+        return await self.get_schema.from_queryset_single(self.model.get(**kwargs))
+
+    async def delete(self, **kwargs) -> None:
+        obj = await self.model.filter(**kwargs).delete()
+        if not obj:
+            raise HTTPException(status_code=404, detail=f"Object not found")
