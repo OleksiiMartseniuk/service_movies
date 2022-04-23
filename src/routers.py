0e5bc36d0ab12d949e6182a5232cd8a22aff4296
Aleksey Martsenyk <martsenyk.l@gmail.com>
2022-04-23 14:10:30 +0300

    add pagination

--- src/routers.py
+++ src/routers.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
-from src.app.films.endpoint import search
+from src.app.films.endpoint import imdb
+from fastapi_pagination import add_pagination
 
 films = APIRouter()
+add_pagination(imdb.imdb_router)
 
-films.include_router(search.search_router, tags=['search'])
+films.include_router(imdb.imdb_router, tags=['imdb'])
