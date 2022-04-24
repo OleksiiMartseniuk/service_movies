4e8e8dbe92961821b124ea08f00e8c9ec91c768f
Aleksey Martsenyk <martsenyk.l@gmail.com>
2022-04-24 17:54:14 +0300

    create schemas

---
+++ src/app/auth/schemas.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+from tortoise.contrib.pydantic import pydantic_model_creator
+
+from src.app.auth import models
+
+
+User = pydantic_model_creator(models.User, name="get_user", exclude=('hashed_password',))
+
+
+class TokenPayload(BaseModel):
+    user_id: int = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class UserName(BaseModel):
+    username: str
