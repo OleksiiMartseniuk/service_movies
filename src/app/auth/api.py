3f84d646c9022e78be76caeaf9dd6f38768bba72
Aleksey Martsenyk <martsenyk.l@gmail.com>
2022-04-24 17:54:47 +0300

    add user_router

---
+++ src/app/auth/api.py
@@ -0,0 +1,18 @@
+from fastapi import Depends, APIRouter
+from fastapi.security import OAuth2PasswordRequestForm
+
+from src.app.auth import schemas, models
+from src.app.auth.auth import get_current_active_user
+from src.app.auth import service
+
+user_router = APIRouter()
+
+
+@user_router.post("/token", response_model=schemas.Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    return await service.user_s.auth(form_data.username, form_data.password)
+
+
+@user_router.get("/user/me", response_model=schemas.User)
+async def read_users_me(current_user: models.User = Depends(get_current_active_user)):
+    return current_user
