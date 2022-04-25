5610707657e10875c0512e5fd2582b2330ac9e51
Aleksey Martsenyk <martsenyk.l@gmail.com>
2022-04-25 22:31:17 +0300

    add model Person

---
+++ src/app/person/models.py
@@ -0,0 +1,13 @@
+from tortoise import fields, models
+
+
+class Person(models.Model):
+    """ Модель человека """
+    id_person = fields.CharField(max_length=15)
+    name = fields.CharField(max_length=100)
+    image = fields.CharField(max_length=1000, null=True)
+    as_character = fields.CharField(max_length=150, null=True)
+    # directors: fields.ManyToManyRelation['FilmReel']
+    # writers: fields.ManyToManyRelation['FilmReel']
+    # stars: fields.ManyToManyRelation['FilmReel']
+    # actors: fields.ManyToManyRelation['FilmReel']
