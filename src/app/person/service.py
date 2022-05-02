3aed3b6c25f51c730d1c3ed5ccf2f07e3a436f1b
Aleksey Martsenyk <martsenyk.l@gmail.com>
2022-05-02 18:21:00 +0300

    edit person m2m schemas

--- src/app/person/service.py
+++ src/app/person/service.py
@@ -12,19 +12,19 @@ class ServicePerson(ServiceCRUD):
     model = models.Person
     model_film = FilmReel
 
-    async def list_for_film_reel(self, person: models.ModelName, id_film_reel: int) -> List[models.Person]:
+    async def list_for_film_reel(self, person: models.ModelName, id_film_reel: int) -> List[schemas.GetPerson]:
         film_reel = await self.model_film.get(id=id_film_reel)
         if person.value == 'director':
-            list_person = await film_reel.director.all()
+            list_person = film_reel.director
         elif person.value == 'writer':
-            list_person = await film_reel.writer.all()
+            list_person = film_reel.writer
         elif person.value == 'star':
-            list_person = await film_reel.star.all()
+            list_person = film_reel.star
         elif person.value == 'actor':
-            list_person = await film_reel.actor.all()
+            list_person = film_reel.actor
         else:
             raise HTTPException(status_code=404, detail=f"person not valid")
-        return list_person
+        return await self.get_schema.from_queryset(list_person.all())
 
 
 person_s = ServicePerson()
