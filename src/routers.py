ca1b96ef9d2dd00e63162fb2fb26035be4256013
Aleksey Martsenyk <martsenyk.l@gmail.com>
2022-05-02 18:20:25 +0300

    add user_router

--- src/routers.py
+++ src/routers.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from src.app.films.endpoint import imdb
 from src.app.auth import api as api_auth
 from src.app.person import api as api_person
+from src.app.user import api as api_user
 
 
 films = APIRouter()
@@ -10,3 +11,4 @@ films = APIRouter()
 films.include_router(imdb.imdb_router, prefix='/imdb', tags=['imdb'])
 films.include_router(api_auth.user_router, tags=['auth'])
 films.include_router(api_person.person_router, tags=['person'])
+films.include_router(api_user.user_router, tags=['user'])
