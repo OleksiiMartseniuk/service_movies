2c735b5df3da99d0a098f13bb8b750ff93e68d12
Aleksey Martsenyk <martsenyk.l@gmail.com>
2022-05-07 00:30:08 +0300

    edit test settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -13,11 +13,18 @@ HOST_DB = os.getenv('HOST_DB')
 PORT_DB = os.getenv('PORT_DB')
 
 DATABASE_URI = os.getenv('DATABASE_URI')
+DATABASE_TEST_URL = 'sqlite://:memory:'
 
 ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:4200/",
     "http://localhost:8080/"
+]
+
+APPS_MODELS_TEST = [
+    "src.app.films.models",
+    "src.app.auth.models",
+    "src.app.person.models",
 ]
 
 APPS_MODELS = [
@@ -37,7 +44,7 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 DATA_IMDB_DIR = os.path.join(BASE_DIR, 'src/service/imdb/data')
-DATA_IMDB_DIR_TEST = os.path.join(BASE_DIR, 'src/tests/test_write_json/data_test')
+DATA_IMDB_DIR_TEST = os.path.join(BASE_DIR, 'src/tests/write_json/data_test')
 
 PATH_GROUP_FILE = os.path.join(DATA_IMDB_DIR, 'group_movies.json')
 PATH_MOVIES_FILE = os.path.join(DATA_IMDB_DIR, 'movies.json')
