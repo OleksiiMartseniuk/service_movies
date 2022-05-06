095d8f9d862d7defa9355003b66fae3b6c32a27e
Aleksey Martsenyk <martsenyk.l@gmail.com>
2022-05-07 00:32:03 +0300

    edit model Person

--- src/app/person/models.py
+++ src/app/person/models.py
@@ -15,12 +15,6 @@ class Person(models.Model):
     name = fields.CharField(max_length=100)
     image = fields.CharField(max_length=1000, null=True)
     as_character = fields.CharField(max_length=150, null=True)
-    # directors: fields.ManyToManyRelation['FilmReel']
-    # writers: fields.ManyToManyRelation['FilmReel']
-    # stars: fields.ManyToManyRelation['FilmReel']
-    # actors: fields.ManyToManyRelation['FilmReel']
 
     def __str__(self):
         return self.id_person
-
-    from enum import Enum
